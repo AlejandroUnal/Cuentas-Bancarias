9cfa5f2482304d5d99dc2147f346c69e1735bfa3
Estudiante <Estudiante@10.220.4.77>
2024-11-21 15:13:57 -0500

    PRIMER CARGA DEL CODIGO DE CUENTA BANCARIA

---
+++ src/ejemplo1/Ejemplo1.java
@@ -0,0 +1,27 @@
+
+package ejemplo1;
+
+/**
+ *
+ * @author Néstor Bolívar
+    
+ */
+public class Ejemplo1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Cliente Nestor = new Cliente();
+        
+        Cuenta cuenta1 = new Cuenta(Nestor);
+        
+        
+
+      
+        
+    }
+ 
+
+}
